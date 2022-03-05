64421237cd0dda219d8a6aa1b95894ee72b53819
jdmaradiago2000 <jmaradiago2000@gmail.com>
2022-03-05 16:18:28 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import './styles.css'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [pokemon, setPokemon] = useState ({})
+  const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [isDc, setIsDc] = useState(true)
+  const [isHc, setIsHc] = useState(true)
+  const [counter, setCounter] = useState(1);
+  
+
+    useEffect(() => {
+      axios.get (`https://pokeapi.co/api/v2/pokemon/${counter}/`)
+        .then(res => {
+          setPokemon(res.data);
+          setHeight(res.data.height);
+          setWeight(res.data.weight);
+        }); 
+    }, [counter]);
+
+    const convertHeight = () => {
+      if(isDc){
+        setHeight(height/10)
+        setIsDc(false);
+      }else{
+        setHeight(height*10)
+        setIsDc(true);
+      }
+    }
+
+    const convertWeight = () => {
+      if(isHc){
+        setWeight(weight/10)
+        setIsHc(false);
+      }else{
+        setWeight(weight*10)
+        setIsHc(true);
+      }  
+    }
+    
+    return (
+      <div className="App">
+        <div className="pokemon-card">
+          <h1>{pokemon.name?.toUpperCase()}</h1>
+          <img src={pokemon.sprites?.other.dream_world.front_default} alt="" />
+          <ul>
+            <li><b>Weight: </b> {weight} {isHc ? "Hectograms" : "Kilograms"} </li>
+            <li><b>Height: </b> {height} {isDc ? "Decimeters" : "Meters"} </li>
+            <li><b>Type: </b> {pokemon.types?.[0].type.name} </li>
+          </ul>
+          <button onClick={convertWeight}>Convert to Kilograms</button>
+          <button onClick={convertHeight}>Convert to Meters</button>
+
+          <button onClick={()=> setCounter (counter - 1)}>Back</button>
+          <button onClick={()=> setCounter (counter + 1)}>Next</button>
+        </div>
     </div>
   );
 }
